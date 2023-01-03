90423fd9f10b056e1cc91e3ac2ca6031d6188642
AndyLy00 <andreia.cristina.siretanu@gmail.com>
2023-01-03 13:27:20 +0200

    New version

--- src/pages/Create.tsx
+++ src/pages/Create.tsx
@@ -26,31 +26,14 @@ export const Create = () => {
                             <input type="text" className="advanced_input"/>
                         </div>
                     </div>
-                </div>
-
-                <div className="third_line">
-                    <div className="title"> Tags</div>
-                    <div className="type_of_border">
-                        <div className="tags_title"> Filter tags</div>
-                        <div className="checkbox">
-                            <div className="checkbox_tag"><input type="checkbox" className="chose"></input>
-                                <div className="chose_name">#Tag1</div>
-                            </div>
-                            <div className="checkbox_tag"><input type="checkbox" className="chose"></input>
-                                <div className="chose_name">#Tag2</div>
-                            </div>
-                            <div className="checkbox_tag"><input type="checkbox" className="chose"></input>
-                                <div className="chose_name">#Tag3</div>
-                            </div>
-                            <div className="checkbox_tag"><input type="checkbox" className="chose"></input>
-                                <div className="chose_name">#Tag4</div>
-                            </div>
-                            <div className="checkbox_tag"><input type="checkbox" className="chose"></input>
-                                <div className="chose_name">#Tag5</div>
-                            </div>
+                    <div className="advanced_URL">
+                        <div className="title">Tags</div>
+                        <div>
+                            <input type="text" className="advanced_input"/>
                         </div>
                     </div>
                 </div>
+
             </div>
             <button className="submit submit2">Create</button>
         </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,68 +1,52 @@
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faCircleInfo} from '@fortawesome/free-solid-svg-icons'
 import {faCirclePlus} from '@fortawesome/free-solid-svg-icons'
-import {JSXElementConstructor, Key, ReactElement, ReactFragment, ReactPortal, useEffect, useState} from "react";
-import axios from "axios";
-// import { ReactElement, JSXElementConstructor, ReactFragment, ReactPortal } from 'react'
+import {JSXElementConstructor, ReactElement, ReactFragment, ReactPortal, useEffect, useState} from "react";
+import getApi from "../data/data";
 
 export const Home = () => {
     const element = <FontAwesomeIcon icon={faCircleInfo}/>
     const element2 = <FontAwesomeIcon icon={faCirclePlus}/>
-    const main_url = 'http://127.0.0.1:5000/bookmarks';
     const [bookmarks, setBookmark] = useState<any>([]);
 
+    const getData = async () => {
+        const {data} = await getApi.get("/");
+        setBookmark(data);
+    };
+
     useEffect(() => {
-        fetchData();
-    }, [])
+        getData().then();
+    }, []);
 
-    const fetchData = async () => {
-        const {data} = await axios.get(main_url)
-        setBookmark(data)
-    }
+    const DisplayData = bookmarks.map(
+        (bookmark: any) => {
+            return (
+                <div className="bookmark_element">
+                    <div className="bookmark_number"> {bookmark.id} </div>
+                    <div className="bookmark_info">
+                        <div className="top">
 
+                            <div className="left">
+                                <div className="bookmark_name">{bookmark.title}</div>
+                            </div>
 
-    //
-    // useEffect(() => {
-    //     getData();
-    // }, [])
-    //
-    // async function getData() {
-    //
-    //     fetch(main_url)
-    //         .then((response) => response.json())
-    //         .then((data) => console.log(data));
-    // }
+                            <div className="right">
+                                {bookmark.tag.map((tag: string) => (
+                                    <div className="bookmark_tags">{tag} </div>
+                                ))}
+                                <div className="info_logo">{element}</div>
+                            </div>
 
+                        </div>
+                        <div className="bottom">
+                            <div className="bookmark_URL"> {bookmark.url}</div>
+                        </div>
+                    </div>
+                </div>
 
-    // const data = require('../data/data.json');
-    //
-    // const DisplayData = data.bookmarks.map(
-    //     (bookmark: any) => {
-    //         return (
-    //             <div className="bookmark_element">
-    //                 <div className="bookmark_number"> {bookmark.id} </div>
-    //                 <div className="bookmark_info">
-    //                     <div className="top">
-    //
-    //                         <div className="left">
-    //                             <div className="bookmark_name">{bookmark.title}</div>
-    //                         </div>
-    //
-    //                         <div className="right">
-    //                             <div className="bookmark_tags">{bookmark.tags}</div>
-    //                             <div className="info_logo">{element}</div>
-    //                         </div>
-    //
-    //                     </div>
-    //                     <div className="bottom">
-    //                         <div className="bookmark_URL"> {bookmark.url}</div>
-    //                     </div>
-    //                 </div>
-    //             </div>
-    //
-    //         )
-    //     }
-    // )
+            )
+        }
+    )
 
     return (
         <>
@@ -71,37 +55,14 @@ export const Home = () => {
                     <div className="list"> Most popular
                         <button className="edit">Edit</button>
                     </div>
-                    {bookmarks.map((bookmark: { id: React.Key | null | undefined; title: string; description: string; url: string; }) => (
-                    <div key={bookmark.id}>
-                        <div className="bookmark_element">
-                            <div className="bookmark_number"> {bookmark.id} </div>
-                            <div className="bookmark_info">
-                                <div className="top">
-
-                                    <div className="left">
-                                        <div className="bookmark_name">{bookmark.title}</div>
-                                    </div>
-
-                                    <div className="right">
-                                        <div className="bookmark_tags">{bookmark.description}</div>
-                                        <div className="info_logo">{element}</div>
-                                    </div>
-
-                                </div>
-                                <div className="bottom">
-                                    <div className="bookmark_URL"> {bookmark.url}</div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                        ))}
+                    <div className="data">{DisplayData}</div>
                 </div>
 
                 <div>
                     <div className="list"> Recent
                         <button className="edit">Edit</button>
                     </div>
-                    {/*<div className="data">{DisplayData}</div>*/}
+                    <div className="data">{DisplayData}</div>
                 </div>
             </div>
 
