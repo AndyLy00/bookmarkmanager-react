0cdd70470c0578e36576514cdcba9718afbf9b66
AndyLy00 <andreia.cristina.siretanu@gmail.com>
2023-01-10 13:57:29 +0200

    Final Version

--- src/pages/Advanced.tsx
+++ src/pages/Advanced.tsx
@@ -10,18 +10,9 @@ export const Advanced = () => {
     const [tag, setTag] = useState('');
 
     const [data, setData] = useState<any[]>([]);
-    const [bookmarks, setBookmark] = useState<any>([]);
     const [newTag, setNewTag] = useState<any[]>([]);
     const element = <FontAwesomeIcon icon={faCircleInfo}/>
 
-    const getData = async () => {
-        const {data} = await getApi.get("/");
-        setBookmark(data);
-    };
-
-    useEffect(() => {
-        getData().then();
-    }, []);
 
     async function SearchByTitle() {
         const {data} = await getApi.get("/");
@@ -64,7 +55,7 @@ export const Advanced = () => {
         (newt: any) => {
             return (
                 <>
-                        <option className="bookmark_tags">{newt}</option>
+                    <option className="bookmark_tags">{newt}</option>
                 </>
             )})
 
@@ -86,83 +77,100 @@ export const Advanced = () => {
 
     return (
         <>
-        <div className="container">
-            <div className="data" style={{paddingBottom: "60px"}}>
-                <div className="description_data">Enter your search criteria</div>
-                <div className="first_line">
-                    <div className="title_name">
-                        <div className="title"> Title</div>
-                        <div>
-                            <input type="text" className="advanced_input" value={title} onChange={e => setTitle(e.target.value)}/>
-                        </div>
-                    </div>
-
-                    <div className="description">
-                        <div className="title"> Description</div>
-                        <div>
-                            <input type="text" className="advanced_input" value={description} onChange={e => setDescription(e.target.value)}/>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="second_line">
-                    <div className="advanced_URL">
-                        <div className="title">URL</div>
-                        <div>
-                            <input type="text" className="advanced_input" value={url} onChange={e => setUrl(e.target.value)}/>
-                        </div>
-                    </div>
-
-                    <div className="advanced_tags">
-                        <div className="title">Tag</div>
-                        <div>
-                            <select className="advanced_input" value={tag} onChange={e => setTag(e.target.value)}>
-                                <option className="bookmark_tags"></option>
-                                {SpecialInput}
-                            </select>
-                        </div>
-                    </div>
-
-                </div>
-                <button className="submit" onClick={() => {
-                SearchByTitle().then()
-                SearchByDescription().then()
-                SearchByUrl().then()
-                SearchByTag().then()}
-                }>Submit</button>
-            </div>
-
-            <div className="data" style={{marginTop: "30px"}}>
-                {data.map(item => {
-                    return (
-                        <div className="bookmark_element" key={item.id}>
-                            <div className="bookmark_info">
-                                <div className="top">
-
-                                    <div className="left">
-                                        <div className="bookmark_name">{item.title}</div>
-                                    </div>
-
-                                    <div className="right">
-                                        {item.tags.map((tag: string) => (
-                                            <div className="bookmark_tags">{tag}</div>
-                                        ))}
-                                        <div className="info_logo">{element}</div>
-                                    </div>
-
-                                </div>
-                                <div className="bottom">
-                                    <a className="bookmark_URL" href={item.url}> {item.url}</a>
-                                </div>
+            <div className="container">
+                <div className="data">
+                    <div className="description_data">Enter your search criteria</div>
+                    <div className="first_line">
+                        <div className="title_name">
+                            <div className="title"> Title</div>
+                            <div>
+                                <input type="text" className="advanced_input" value={title} onChange={e => setTitle(e.target.value)}/>
+                                <button className="submit newSubmit" onClick={() => {
+                                    SearchByTitle().then()
+                                }
+                                }>Submit</button>
                             </div>
-                            <button className="delete_button" onClick={deleteDataById.bind(this, item.id)}>X</button>
                         </div>
-                    )
-                })}
-            </div>
 
-            <div className="space"></div>
-        </div>
+                        <div className="description">
+                            <div className="title"> Description</div>
+                            <div>
+                                <input type="text" className="advanced_input" value={description} onChange={e => setDescription(e.target.value)}/>
+                                <button className="submit newSubmit" onClick={() => {
+                                    SearchByDescription().then()
+                                }
+                                }>Submit</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="second_line">
+                        <div className="advanced_URL">
+                            <div className="title">URL</div>
+                            <div>
+                                <input type="text" className="advanced_input" value={url} onChange={e => setUrl(e.target.value)}/>
+                                <button className="submit newSubmit" onClick={() => {
+                                    SearchByUrl().then()
+                                }
+                                }>Submit</button>
+                            </div>
+                        </div>
+
+                        <div className="advanced_tags">
+                            <div className="title">Tag</div>
+                            <div>
+                                <select className="advanced_input" value={tag} onChange={e => setTag(e.target.value)}>
+                                    <option className="bookmark_tags"></option>
+                                    {SpecialInput}
+                                </select>
+                                <button className="submit newSubmit" onClick={() => {
+                                    SearchByTag().then()
+                                }
+                                }>Submit</button>
+                            </div>
+                        </div>
+
+                    </div>
+                    {/*<button className="submit" onClick={() => {*/}
+                    {/*    SearchByTitle().then()*/}
+                    {/*    SearchByDescription().then()*/}
+                    {/*    SearchByUrl().then()*/}
+                    {/*    // SearchByTag().then()*/}
+                    {/*    }*/}
+                    {/*}>Submit</button>*/}
+                </div>
+
+                <div className="data" style={{marginTop: "30px"}}>
+                    {data.map((newItem: any) => {
+                        return (
+                            <div className="bookmark_element" key={newItem.id}>
+                                <div className="bookmark_info">
+                                    <div className="top">
+
+                                        <div className="left">
+                                            <div className="bookmark_name">{newItem.title}</div>
+                                        </div>
+
+                                        <div className="right">
+                                            {newItem.tags.map((tag: string) => (
+                                                <div className="bookmark_tags">{tag}</div>
+                                            ))}
+                                            <div className="info_logo">{element}</div>
+                                        </div>
+
+                                    </div>
+                                    <div className="bottom">
+                                        <a className="bookmark_URL" href={newItem.url}> {newItem.url}</a>
+                                    </div>
+                                </div>
+                                <button className="delete_button" onClick={deleteDataById.bind(this, newItem.id)}>X</button>
+                            </div>
+                        )
+                    })}
+                </div>
+
+                <div className="space"></div>
+            </div>
 
         </>
     )
